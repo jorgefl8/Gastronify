46cf8dc38580cb733ce225ff29299b19cc2f4ca5
Alejandro <aleferoro@alum.us.es>
2024-02-29 13:30:08 +0100

    Feat: Style profile

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -4,6 +4,7 @@ import { FirebaseAuth, FirestoreDB } from "../../firebase/firebaseconfig.js";
 import { doc, getDoc } from "firebase/firestore";
 import Loading from "../components/Loading.jsx";
 import theme from "../theme.js";
+import { Link } from "react-router-native";
 
 const Profile = (props) => {
   const [user, setUser] = useState(null);
@@ -12,7 +13,11 @@ const Profile = (props) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const currentUser = doc(FirestoreDB, "Users", FirebaseAuth.currentUser.uid);
+        const currentUser = doc(
+          FirestoreDB,
+          "Users",
+          FirebaseAuth.currentUser.uid
+        );
         const userDoc = await getDoc(currentUser);
         if (userDoc.exists()) {
           setUser(userDoc.data());
@@ -28,27 +33,40 @@ const Profile = (props) => {
 
     fetchData();
   }, []);
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <View style={styles.container}>
-      {loading ? (
-        <Loading />
-      ) : (
-        <View style={styles.profileInfo}>
-          <Text style={styles.infoText}>Name: {user ? user.Name : "User data not available yet"}</Text>
-          <Text style={styles.infoText}>Last name: {user? user.LastName : "Not available yet"}</Text>
-          <Text style={styles.infoText}>Email:{user ? user.Email : "Email not available yet"}</Text>
-          <Text style={styles.infoText}>Telephone Number: {user ? user.TelephoneNumber : "Not available yet"}</Text>
-          <View style={styles.buttonsContainer}>
-            <View style={styles.buttonWrapper}>
-              <Button title="Modify Data" style={styles.button} ></Button>
-            </View>
-            <View style={styles.buttonWrapper}>
-              <Button title="Log Out" onPress={props.handleLogout} />
-            </View>
+      <View style={styles.profileInfo}>
+        <Text style={styles.infoText}>
+          Name: {user ? user.Name : "User data not available yet"}
+        </Text>
+        <Text style={styles.infoText}>
+          Last name: {user ? user.LastName : "Not available yet"}
+        </Text>
+        <Text style={styles.infoText}>
+          Email:{user ? user.Email : "Email not available yet"}
+        </Text>
+        <Text style={styles.infoText}>
+          Telephone Number: {user ? user.TelephoneNumber : "Not available yet"}
+        </Text>
+        <View style={styles.buttonsContainer}>
+          <Link title="Modify Data" style={styles.button}>
+            <Text style={styles.buttonText}>Modify data</Text>
+          </Link>
+          <View style={styles.button}>
+            <Text
+              style={styles.buttonText}
+              title="Log Out"
+              onPress={props.handleLogout}
+            >
+              Log out
+            </Text>
           </View>
         </View>
-      )}
+      </View>
     </View>
   );
 };
@@ -60,12 +78,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
+    marginVertical: 200
   },
   profileInfo: {
     alignItems: "center",
     backgroundColor: theme.colors.backgroundColor,
     borderRadius: 10,
     padding: 20,
+    justifyContent: "center",
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -81,21 +101,22 @@ const styles = StyleSheet.create({
   },
   buttonsContainer: {
     flexDirection: "row",
-    marginTop: 20,
-  },
-  buttonWrapper: {
+    marginTop: 3,
+    alignItems: "center",
     flex: 1,
-    marginHorizontal: 5,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+  },
+  button: {
+    flex: 1,
+    backgroundColor: theme.colors.secondary,
+    padding: 8,
+    borderRadius: 5,
+    margin: 6,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
   },
 });
-
 
 export default Profile;
