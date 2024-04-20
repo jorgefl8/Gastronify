2b5537de676e16e392026dbbfba2a93fcc4e9a38
jorgeflorentinomarbe <jorgeflorentinomarbe@gmail.com>
2024-04-20 17:17:54 +0200

    fix: booksform now working well

--- src/screens/BooksForm.jsx
+++ src/screens/BooksForm.jsx
@@ -1,118 +1,161 @@
 import React, { useState } from 'react';
-import { ScrollView, TouchableOpacity, StyleSheet, Text, Alert } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 import { Calendar } from 'react-native-calendars';
-import { TextInput } from 'react-native';
-import { View } from 'react-native-animatable';
+import RNPickerSelect from 'react-native-picker-select';  // Importación del componente
+import Icon from 'react-native-vector-icons/Feather';  // Asegúrate de tener esta librería instalada
 import theme from '../theme.js';
-import { Picker } from '@react-native-picker/picker';
 import functions from "../../firebase/firebaseUtils.js";
+import { FirebaseAuth } from "../../firebase/firebaseconfig.js";
+
 
 const BooksForm = () => {
+
     const [selectedDate, setSelectedDate] = useState(null);
     const [name, setName] = useState('');
     const [phone, setPhone] = useState('');
-    const [people, setPeople] = useState(1);
-    const [time, setTime] = useState('12:00');
+    const [people, setPeople] = useState("1");
+    const [time, setTime] = useState('13:00');
 
     const handleDateSelect = (date) => {
         setSelectedDate(date.dateString);
     };
-
-    const handleSubmit = () => {
+    const validateForm = () => {
+        const trimmedName = name.trim();
+        const trimmedPhone = phone.trim();
+        if (selectedDate === null) {
+            Alert.alert("Validation Error", "Please select a date.");
+            return false;
+        }
+        if (trimmedName.length === 0) {
+          Alert.alert("Validation Error", "Please enter a name.");
+          return false;
+        }
+        if (trimmedPhone.length != 9) {
+          Alert.alert("Validation Error", "Please enter a valid phone number with at least 10 digits.");
+          return false;
+        }
+        return true; 
+      };
+      
+      const handleSubmit = () => {
+        if (!validateForm()) {
+          return;
+        }
         console.log('Selected Date:', selectedDate);
         console.log('Name:', name);
         console.log('Phone:', phone);
         console.log('People:', people);
         console.log('Time:', time);
         const DataBooking = { "Date": selectedDate, "Name": name, "PhoneNumber": phone, "Hour": time, "People": people };
-        functions.uploadDoc("Bookings", DataBooking);
-        Alert.alert(
-            "Reservation Completed",
-            "We are waiting for you!",
-            [{ text: "OK", onPress: () => console.log("OK Pressed") }]
-        );
-    };
-    return (
-            <View style={styles.container}>
-                <Calendar onDayPress={handleDateSelect} style={styles.calendarview}
-                    markedDates={{ [selectedDate]: { selected: true, selectedColor: theme.colors.secondary } }} />
-                <View style={styles.separator} />
-                <View style={styles.campos}>
-                    <Text style={styles.text}>Name: </Text>
-                    <TextInput
-                        placeholder="Enter your name"
-                        value={name}
-                        onChangeText={(text) => setName(text)}
-                        style={styles.input}
-                    />
-                </View>
-                <View style={styles.separator} />
-                <View style={styles.campos}>
-                    <Text style={styles.text}>Phone Number: </Text>
-                    <TextInput
-                        placeholder="Enter your phone number"
-                        value={phone}
-                        onChangeText={(text) => setPhone(text)}
-                        style={styles.input}
-                    />
-                </View>
-                <View style={styles.separator} />
-                <View style={styles.campos}>
-                    <Text style={styles.text}>Number of people: </Text>
-                    <Picker selectedValue={people} onValueChange={(itemValue) => setPeople(itemValue)}
-                        style={styles.picker}>
-                        <Picker.Item label="1" value="1" />
-                        <Picker.Item label="2" value="2" />
-                        <Picker.Item label="3" value="3" />
-                        <Picker.Item label="4" value="4" />
-                        <Picker.Item label="5" value="5" />
-                        <Picker.Item label="6" value="6" />
-                        <Picker.Item label="7" value="7" />
-                        <Picker.Item label="8" value="8" />
-                        <Picker.Item label="9" value="9" />
-                        <Picker.Item label="10" value="10" />
-                    </Picker>
-                </View>
-                <View style={styles.separator} />
-                <View style={styles.campos}>
-                    <Text style={styles.text}>Reservation Time:  </Text>
-                    <Picker selectedValue={time} onValueChange={(itemValue) => setTime(itemValue)}
-                        style={styles.picker} itemStyle={styles.itemStyle}>
-                        <Picker.Item label="13:00" value="13:00" />
-                        <Picker.Item label="13:30" value="13:30" />
-                        <Picker.Item label="14:00" value="14:00" />
-                        <Picker.Item label="14:30" value="14:30" />
-                        <Picker.Item label="15:00" value="15:00" />
-                        <Picker.Item label="15:30" value="15:30" />
-                        <Picker.Item label="21:00" value="21:00" />
-                        <Picker.Item label="21:30" value="21:30" />
-                        <Picker.Item label="22:00" value="22:00" />
-                        <Picker.Item label="22:30" value="22:30" />
-                        <Picker.Item label="23:00" value="23:00" />
-                    </Picker>
-                </View>
+        functions.uploadDoc("Users", DataBooking)
+          .then(() => {
+            Alert.alert("Reservation Completed", "We are waiting for you!", [{ text: "OK", onPress: () => console.log("OK Pressed") }]);
+          })
+          .catch(error => {
+            console.error("Submission Error", error);
+            Alert.alert("Error", "Failed to complete the reservation.");
+          });
+      };
+      
 
-                <View style={styles.separator} />
-                {/* <Button title="Book" onPress={handleSubmit} style={styles.button} /> */}
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-                        <Text style={styles.buttonText}>BOOK</Text>
-                    </TouchableOpacity>
-                </View>
-                
+    return (
+        <View style={styles.container}>
+            <Calendar onDayPress={handleDateSelect} style={styles.calendarview}
+                markedDates={{ [selectedDate]: { selected: true, selectedColor: theme.colors.secondary } }} />
+            <View style={styles.separator} />
+            <View style={styles.campos}>
+                <Text style={styles.text}>Name: </Text>
+                <TextInput
+                    placeholder="Enter your name"
+                    value={name}
+                    onChangeText={setName}
+                    style={styles.input}
+                />
             </View>
+            <View style={styles.separator} />
+            <View style={styles.campos}>
+                <Text style={styles.text}>Phone Number: </Text>
+                <TextInput
+                    placeholder="Enter your phone number"
+                    value={phone}
+                    onChangeText={setPhone}
+                    style={styles.input}
+                />
+            </View>
+            <View style={styles.separator} />
+            <View>
+                <Text style={styles.label}>Number of people:</Text>
+                <RNPickerSelect
+                    onValueChange={(value) => setPeople(value)}
+                    items={[
+                        { label: "1", value: "1" },
+                        { label: "2", value: "2" },
+                        { label: "3", value: "3" },
+                        { label: "4", value: "4" },
+                        { label: "5", value: "5" }
+                    ]}
+                    value={people}
+                    useNativeAndroidPickerStyle={false} // use this to style as needed
+                    style={pickerSelectStyles}
+                    Icon={() => {
+                        return <Icon name="chevron-down" size={24} color="gray" />;
+                    }}
+                />
+            </View>
+            <View style={styles.separator} />
+            <View>
+                <Text style={styles.label}>Reservation Time:</Text>
+                <RNPickerSelect
+                    onValueChange={(value) => setTime(value)}
+                    items={[
+                        { label: "13:00", value: "13:00" },
+                        { label: "13:30", value: "13:30" },
+                        { label: "14:00", value: "14:00" },
+                        { label: "14:30", value: "14:30" },
+                        { label: "15:00", value: "15:00" },
+                        { label: "15:30", value: "15:30" },
+                        { label: "20:00", value: "20:00" },
+                        { label: "20:30", value: "20:30" },
+                        { label: "21:00", value: "21:00" },
+                        { label: "21:30", value: "21:30" },
+                        { label: "22:00", value: "22:00" },
+                        { label: "22:30", value: "22:30" },
+                        { label: "23:00", value: "23:00" },
+                        { label: "23:30", value: "23:30" }
+                    ]}
+                    value={time}
+                    useNativeAndroidPickerStyle={false} // use this to style as needed
+                    style={pickerSelectStyles}
+                    Icon={() => {
+                        return <Icon name="chevron-down" size={24} color="gray" />;
+                    }}
+                />
+            </View>
+            <View style={styles.separator} />
+            <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                <Text style={styles.buttonText}>BOOK</Text>
+            </TouchableOpacity>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
-    calendarview: {
-        borderRadius: 10,
-    },
     container: {
         flexGrow: 1,
         padding: 12,
         margin: 8,
         marginBottom: 0,
+    },
+    calendarview: {
+        borderRadius: 10,
+    },
+    campos: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    label: {
+        fontSize: 16,
+        marginBottom: 5,
     },
     text: {
         color: theme.colors.textPrimary,
@@ -127,39 +170,50 @@ const styles = StyleSheet.create({
         padding: 10,
         flex: 1,
     },
-    campos: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
     separator: {
-        height: 15,
+        height: 20,
     },
-    picker: {
-        height: 40,
-        flex: 1,
-        color: 'rgba(0, 0, 0, 0.5)', // Color del texto con transparencia
-        backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    },
-    itemStyle: {
-        backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    },
-    buttonContainer: {
-        alignItems: 'center', // Alinea el botón en el centro horizontalmente
-        marginTop: 15, // Espacio entre el botón y otros elementos si es necesario
-      },
-      button: {
+    button: {
         backgroundColor: theme.colors.secondary,
-        paddingVertical: 12, // Ajusta el espacio vertical
-        paddingHorizontal: 25, // Ajusta el espacio horizontal
-        borderRadius: 8,
+        padding: 10,
+        borderRadius: 5,
         alignItems: 'center',
         justifyContent: 'center',
-      },
-      buttonText: {
+    },
+    buttonText: {
         color: 'white',
         fontSize: 18,
-        fontWeight: theme.fontWeights.bold,
-      },
+        fontWeight: 'bold',
+    },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        fontSize: 16,
+        paddingVertical: 12,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 4,
+        color: 'black',
+        paddingRight: 30, // to ensure the text is never behind the icon
+        backgroundColor: 'white',
+    },
+    inputAndroid: {
+        fontSize: 16,
+        paddingHorizontal: 10,
+        paddingVertical: 8,
+        borderWidth: 0.5,
+        borderColor: 'purple',
+        borderRadius: 8,
+        color: 'black',
+        paddingRight: 30, // to ensure the text is never behind the icon
+        backgroundColor: 'white',
+    },
+    iconContainer: {
+        top: 5,
+        right: 15,
+    },
 });
 
 export default BooksForm;
