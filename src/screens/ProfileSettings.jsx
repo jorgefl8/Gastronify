8da3598a5c91f423b3d12caf901d6af1a879b68d
PABLO VERA LOPEZ <pabverlop1@alum.us.es>
2024-04-22 16:48:57 +0200

    add: new Personal Data view

--- src/screens/ProfileSettings.jsx
+++ src/screens/ProfileSettings.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput, Button,Platform  } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput, Button } from "react-native";
 import { FirebaseAuth } from "../../firebase/firebaseconfig.js";
 import functions from "../../firebase/firebaseUtils.js";
 import Loading from "../components/Loading.jsx";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import theme from "../theme.js";
+import moment from "moment";
 
 const ProfileSettings = (props) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [modalVisible, setModalVisible] = useState(false);
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
 
   useEffect(() => {
     const fetchMenu = async () => {
@@ -18,8 +22,7 @@ const ProfileSettings = (props) => {
       );
       setLoading(false);
       setUser(DatUser);
-      // Llama a la función de guardar datos del usuario en Main
-      //props.saveUserData(DatUser);
+      setSelectedDate(DatUser?.Date?.toDate());
     };
 
     fetchMenu();
@@ -27,7 +30,6 @@ const ProfileSettings = (props) => {
 
   const handleSaveChanges = async () => {
     try {
-      // Actualiza los datos del usuario localmente en el estado
       await functions.updateDocByUid("Users", FirebaseAuth.currentUser.uid, user);
       setModalVisible(false);
     } catch (error) {
@@ -42,30 +44,100 @@ const ProfileSettings = (props) => {
     });
   };
 
+  const formatDate = (date) => {
+    return moment(date).format("DD/MM/YYYY");
+  };
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    setSelectedDate(date);
+    handleChange("Date", date);
+    hideDatePicker();
+  };
+
   if (loading) {
     return <Loading />;
   }
 
   return (
     <View style={styles.container}>
-      <View style={styles.Header}>
-        <Text style={{ fontSize: 20 }}>
-          Hi {user ? <Text style={styles.boldText}>{user.Name}</Text> : "Usuario"}!, welcome to your profile info
-        </Text>
-      </View>
       <View style={styles.profileInfo}>
-        <Text style={styles.infoText}>
-          Name: {user ? <Text style={styles.boldText}>{user.Name}</Text> : "Not available yet"}
-        </Text>
-        <Text style={styles.infoText}>
-          Last name: {user ? <Text style={styles.boldText}>{user.LastName}</Text> : "Not available yet"}
-        </Text>
-        <Text style={styles.infoText}>
-          Email: {user ? <Text style={styles.boldText}>{user.Email}</Text> : "Email not available yet"}
-        </Text>
-        <Text style={styles.infoText}>
-          Phone Number: {user ? <Text style={styles.boldText}>{user.TelephoneNumber}</Text> : "Not available yet"}
-        </Text>
+        <Text style={styles.headerText}>Personal Data</Text>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Name</Text>
+          <TextInput
+            style={styles.input}
+            value={user?.Name}
+            onChangeText={(text) => handleChange("Name", text)}
+          />
+        </View>
+        <View style={styles.separator}></View>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Last Name</Text>
+          <TextInput
+            style={styles.input}
+            value={user?.LastName}
+            onChangeText={(text) => handleChange("LastName", text)}
+          />
+        </View>
+        <View style={styles.separator}></View>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            value={user?.Email}
+            onChangeText={(text) => handleChange("Email", text)}
+          />
+        </View>
+        <View style={styles.separator}></View>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Phone Number</Text>
+          <View style={styles.phoneNumberContainer}>
+            <TextInput
+              style={styles.prefixInput}
+              value={user?.Prefix}
+              onChangeText={(text) => handleChange("Prefix", text)}
+              placeholder="Prefix"
+            />
+            <TextInput
+              style={styles.input}
+              value={user?.TelephoneNumber}
+              onChangeText={(text) => handleChange("TelephoneNumber", text)}
+              placeholder="Telephone Number"
+            />
+          </View>
+        </View>
+        <View style={styles.separator}></View>
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Date Birth</Text>
+          <TouchableOpacity onPress={showDatePicker}>
+            <Text style={styles.infoText}>
+              {selectedDate ? formatDate(selectedDate) : "Select date"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.separator}></View>
+
+        {/* Modal para el Date Picker */}
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+        />
+
         <View style={styles.buttonsContainer}>
           <TouchableOpacity
             style={styles.button}
@@ -89,36 +161,52 @@ const ProfileSettings = (props) => {
       >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
-            <TextInput
-              style={styles.input}
-              placeholder="Name"
-              value={user?.Name}
-              onChangeText={(text) => handleChange("Name", text)}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Last Name"
-              value={user?.LastName}
-              onChangeText={(text) => handleChange("LastName", text)}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Email"
-              value={user?.Email}
-              onChangeText={(text) => handleChange("Email", text)}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Telephone Number"
-              value={user?.TelephoneNumber}
-              onChangeText={(text) => handleChange("TelephoneNumber", text)}
-            />
-            <Button title="Save Changes" onPress={handleSaveChanges} />
-            <Button
-              title="Cancel"
-              onPress={() => setModalVisible(false)}
-              color="red"
-            />
+            <Text style={styles.headerText}>Modify Data</Text>
+
+            <View style={styles.infoContainer}>
+              <Text style={styles.label}>Name:</Text>
+              <Text style={styles.infoText}>{user?.Name}</Text>
+            </View>
+
+            <View style={styles.infoContainer}>
+              <Text style={styles.label}>Last Name:</Text>
+              <Text style={styles.infoText}>{user?.LastName}</Text>
+            </View>
+
+            <View style={styles.infoContainer}>
+              <Text style={styles.label}>Email:</Text>
+              <Text
+                style={[styles.infoText, styles.emailText]}
+                numberOfLines={1}
+                ellipsizeMode="tail"
+              >
+                {user?.Email}
+              </Text>
+            </View>
+
+            <View style={styles.infoContainer}>
+              <Text style={styles.label}>Telephone:</Text>
+              <View style={styles.phoneNumberContainer}>
+                <Text style={styles.infoText}>{user?.Prefix}</Text>
+                <Text style={styles.infoText}>{user?.TelephoneNumber}</Text>
+              </View>
+            </View>
+
+            <View style={styles.infoContainer}>
+              <Text style={styles.label}>Date:</Text>
+              <Text style={styles.infoText}>
+                {selectedDate ? formatDate(selectedDate) : "Select date"}
+              </Text>
+            </View>
+
+            <View style={styles.buttonsContainer}>
+              <Button title="Save Changes" onPress={handleSaveChanges} />
+              <Button
+                title="Close"
+                onPress={() => setModalVisible(false)}
+                color="red"
+              />
+            </View>
           </View>
         </View>
       </Modal>
@@ -130,60 +218,55 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: theme.colors.backgroundColor,
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     padding: 20,
   },
-  boldText: {
-    fontWeight: 'bold',
+  headerText: {
+    fontSize: 20,
+    marginBottom: 20,
   },
-  
   profileInfo: {
-    alignItems: "center",
     backgroundColor: theme.colors.backgroundColor,
     borderRadius: 10,
-    padding: 20,
-    justifyContent: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    height: Platform.OS === "ios" ? "37%" : "40%" ,
+    padding: 10,
+  },
+  infoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  label: {
+    width: 120,
+    marginRight: 5,
+    fontSize: 16,
+  },
+  emailText: {
+    flex: 1, // Esto permite que el texto ocupe todo el espacio disponible
+    fontSize: 16,
+    overflow: 'hidden',
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+    borderRadius: 5,
   },
   infoText: {
-    marginBottom: 10,
-    fontSize: 20,
+    fontSize: 16,
   },
   buttonsContainer: {
     flexDirection: "row",
     justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
+    marginTop: 20,
   },
   button: {
-    flex: 1,
     backgroundColor: theme.colors.secondary,
     padding: 8,
     borderRadius: 5,
-    margin: 3,
+    marginHorizontal: 5,
   },
   buttonText: {
     color: "white",
     fontSize: 16,
     textAlign: "center",
-  },
-  Header: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 100, // Ajusta este valor según tus necesidades
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: theme.colors.primary,
   },
   modalContainer: {
     flex: 1,
@@ -197,12 +280,22 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     width: "80%",
   },
-  input: {
-    marginBottom: 10,
+  phoneNumberContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  prefixInput: {
+    width: 60,
+    marginRight: 10,
     padding: 10,
     borderWidth: 1,
     borderColor: "#ccc",
     borderRadius: 5,
+  },
+  separator: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
   },
 });
 
